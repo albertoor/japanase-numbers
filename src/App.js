ee358454750d0981eb53a443e6c334339986ee4b
albertoornelasdev <albertoornelasdev@gmail.com>
2021-09-07 18:05:26 -0700

    Init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Input from "./components/Input/Input";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Convert Numbers and Japanese Numbers</h1>
+      <Input />
+    </div>
+  );
+}
---
+++ src/components/Input/Input.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Input() {
+  return (
+    <>
+      <label htmlFor="number">Enter number</label>
+      <input type="text" />
+    </>
+  );
+}
