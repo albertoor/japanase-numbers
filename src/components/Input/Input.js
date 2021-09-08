180514298bb7d9b32e7735abe8a341c8696ad65d
albertoornelasdev <albertoornelasdev@gmail.com>
2021-09-07 18:34:52 -0700

    first ten numbers

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,10 +1,27 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import japaneseNumbers from "../../constants/japaneseNumbers";
 
 export default function Input() {
+  const [number, setNumber] = useState();
+  const [pronunciation, setPronunciation] = useState("");
+
+  const handleChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const getPronuntiation = () => {
+    setPronunciation(japaneseNumbers[parseInt(number)]);
+  };
+
+  useEffect(() => {
+    getPronuntiation();
+  });
+
   return (
     <>
       <label htmlFor="number">Enter number</label>
-      <input type="text" />
+      <input type="text" value={number} onChange={(e) => handleChange(e)} />
+      <h1>Pronunciation {pronunciation}</h1>
     </>
   );
 }
---
+++ src/constants/japaneseNumbers.js
@@ -0,0 +1,15 @@
+const firstTenNumbers = [
+  "rei",
+  "ichi",
+  "ni",
+  "san",
+  "shi",
+  "go",
+  "roku",
+  "nana",
+  "achi",
+  "kyuu",
+  "juu",
+];
+
+export default firstTenNumbers;
